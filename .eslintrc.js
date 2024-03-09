f79b2b9c04326a712d27a8a450e3d364006b50e2
Sopiriye Jamabo <jayspriye@gmail.com>
2024-03-09 15:19:16 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,17 @@ const eslint = {
     'airbnb',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recrommended',
     'prettier',
   ],
   rules: {
     semi: 2,
+    'prettier/prettier':[
+      'error', 
+      {
+        'endOfLine': 'auto'
+      }
+    ],
     'max-len': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-no-bind': 'off',
